ea4be79c8717917078519b5da17c8baebab25fe5
Thomas Krause <krauseto@hu-berlin.de>
2020-07-09 13:20:16 +0200

    Renamed

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,20 +1,31 @@
 <template>
   <v-app>
     <v-main>
-     <room-description v-for="r in rooms" :key="r.id" :room="r.id" />
+      <div v-if="credentials">
+        <room-overview-link v-for="r in rooms" :key="r.id" :room="r.id" />
+      </div>
+      <div v-else>
+
+        <h1>Login</h1>
+        <form>
+          <v-text-field v-model="user_id" label="User Name" required></v-text-field>
+          <v-text-field v-model="password" label="User Name" required></v-text-field>
+        </form>
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import RoomDescription from "./components/RoomDescription.vue";
+import RoomOverviewLink from "./components/RoomOverviewLink.vue";
+import {Room} from "./models/Room";
 
 export default Vue.extend({
-  components: { RoomDescription },
+  components: { RoomOverviewLink },
   data() {
     return {
-      
+      credentials: null,
       rooms: [
         {
           id: "3.333",
--- webapp/src/index.ts
+++ webapp/src/index.ts
@@ -1,3 +1,11 @@
-export * from './runtime';
-export * from './apis';
-export * from './models';
+import Vue from 'vue';
+import App from './App.vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify)
+
+const vuetify = new Vuetify();
+
+new Vue({ render: createElement => createElement(App), vuetify: vuetify}).$mount('#app');
+
