52a33474dd012e77c7d4df3e99949d5ef79dbf25
Thomas Krause <krauseto@hu-berlin.de>
2020-07-11 11:40:50 +0200

    Login via the REST API

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,17 +1,60 @@
 <template>
   <v-app>
     <v-main>
-      <div v-if="credentials">
+      <v-snackbar
+        v-model="message.show"
+        top = true
+      >
+        {{ message.text }}
+        <v-btn color="pink" flat @click="message.show = false">Close</v-btn>
+      </v-snackbar>
+      <div v-if="user.jwt_token">
         <room-overview-link v-for="r in rooms" :key="r.id" :room="r.id" />
       </div>
-      <div v-else>
+      <v-container v-else class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Login</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-tooltip bottom>
+                  <template v-slot:="{ on }">
+                    <v-btn :href="source" icon large target="_blank" v-on="on">
+                      <v-icon>mdi-code-tags</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Source</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="User Name"
+                    name="login"
+                    prepend-icon="mdi-account"
+                    type="text"
+                    v-model="user.id"
+                  ></v-text-field>
 
-        <h1>Login</h1>
-        <form>
-          <v-text-field v-model="user_id" label="User Name" required></v-text-field>
-          <v-text-field v-model="password" label="User Name" required></v-text-field>
-        </form>
-      </div>
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                    v-model="user.provided_password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn v-on:click="attempt_login" color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -20,12 +63,21 @@
 import Vue from "vue";
 import RoomOverviewLink from "./components/RoomOverviewLink.vue";
 import {Room} from "./models/Room";
+import {RoomplaApi, LoginPostRequest} from "./apis/RoomplaApi";
 
 export default Vue.extend({
   components: { RoomOverviewLink },
   data() {
     return {
-      credentials: null,
+      user: {
+        id: null,
+        provided_password: null,
+        jwt_token: null,
+      },
+      message: {
+        show: false,
+        text: "",
+      },
       rooms: [
         {
           id: "3.333",
@@ -37,6 +89,28 @@ export default Vue.extend({
         }
       ]
     };
+  },
+  methods: {
+    attempt_login: function (event) {
+      let api = new RoomplaApi();
+      let request = {
+        credentials: {
+          userId: this.user.id,
+          password: this.user.provided_password,
+        }
+      };
+      let result = api.loginPost(request);
+      result.then(
+        (response) => {
+          this.user.jwt_token = response; 
+        }
+      ).catch(
+        (response) => {
+          this.message.text = "Login failed: " + response.statusText;
+          this.message.show = true;
+          }
+      );
+    }
   }
 });
 </script>
