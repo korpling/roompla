d613ced6bf744325afbc5eb05dc236b1d4f48479
Thomas Krause <krauseto@hu-berlin.de>
2020-07-11 17:52:08 +0200

    Refactor login form: it is now a separate component

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -3,56 +3,13 @@
     <v-main>
       <v-snackbar v-model="message.show" :top="true">
         {{ message.text }}
-        <v-btn color="pink" text @click="message.show = false">Close</v-btn>
+        <v-btn text @click="message.show = false">Close</v-btn>
       </v-snackbar>
-      <div v-if="api_config.accessToken">
+
+      <div v-if="api.configuration.accessToken">
         <room-overview-link v-for="r in rooms" :key="r.id" :room="r.id" />
       </div>
-      <v-container v-else class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="4">
-            <v-card class="elevation-12">
-              <v-toolbar color="primary" dark flat>
-                <v-toolbar-title>Login</v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-tooltip bottom>
-                  <template v-slot:="{ on }">
-                    <v-btn :href="source" icon large target="_blank" v-on="on">
-                      <v-icon>mdi-code-tags</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Source</span>
-                </v-tooltip>
-              </v-toolbar>
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                    label="User Name"
-                    name="login"
-                    prepend-icon="mdi-account"
-                    type="text"
-                    v-model="credentials.userId"
-                  ></v-text-field>
-
-                  <v-text-field
-                    id="password"
-                    label="Password"
-                    name="password"
-                    prepend-icon="mdi-lock"
-                    type="password"
-                    v-model="credentials.password"
-                    v-on:keyup.enter="attempt_login"
-                  ></v-text-field>
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn v-on:click="attempt_login" color="primary">Login</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
+      <login v-else @logged-in="login_callback" />
     </v-main>
   </v-app>
 </template>
@@ -61,6 +18,7 @@
 import Vue from "vue";
 import VueCookie from "vue-cookie";
 import RoomOverviewLink from "./components/RoomOverviewLink.vue";
+import Login from "./components/Login.vue";
 import { Room } from "./models/Room";
 import { RoomplaApi, LoginPostRequest } from "./apis/RoomplaApi";
 import { Configuration } from "./runtime";
@@ -68,66 +26,42 @@ import { Configuration } from "./runtime";
 Vue.use(VueCookie);
 
 export default Vue.extend({
-  components: { RoomOverviewLink },
+  components: { RoomOverviewLink, Login },
   data() {
     return {
-      api_config: { accessToken: null },
-      credentials: {
-        userId: "",
-        password: null
-      },
+      api: new RoomplaApi(),
+      userId: null,
       message: {
         show: false,
         text: ""
       },
-      rooms: [
-        {
-          id: "3.333",
-          max_occupancy: 2
-        },
-        {
-          id: "3.408",
-          max_occupancy: 1
-        }
-      ]
+      rooms: []
     };
   },
-  created: function() {
-  },
+  created: function() {},
   mounted: function() {
-    if (this.api_config.accessToken) {
+    if (this.api.configuration.accessToken) {
       this.fetch_rooms();
     }
   },
   methods: {
+    login_callback: function(token) {
+      // Recreate the internal API
+      if (token) {
+        this.api = new RoomplaApi(new Configuration({ accessToken: token }));
+        // Since we are now authentifcated, we can fetch the rooms
+        this.fetch_rooms();
+      } else {
+        this.api = new RoomplaApi();
+        this.rooms = [];
+      }
+    },
     fetch_rooms: function() {
-      let config = new Configuration(this.api_config);
-      let api = new RoomplaApi(config);
-      api.roomsGet().then(
+      this.api.roomsGet().then(
         response => (this.rooms = response),
         reason => {
           this.message.text = "Could not fetch rooms: " + reason;
           this.message.show = true;
-        }
-      );
-    },
-    attempt_login: function(event) {
-      let api = new RoomplaApi();
-      let result = api.loginPost({ credentials: this.credentials });
-      result.then(
-        response => {
-          this.api_config.accessToken = response;
-
-          this.credentials.password = null;
-          this.message.show = false;
-
-          // Fetch all rooms from the service, now with authentification
-          this.fetch_rooms();
-        },
-        reason => {
-          this.message.text = "Login failed: " + reason.statusText;
-          this.message.show = true;
-          this.credentials.password = null;
         }
       );
     }
---
+++ webapp/src/components/Login.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-snackbar v-model="message.show" :top="true">
+      {{ message.text }}
+      <v-btn text @click="message.show = false">Close</v-btn>
+    </v-snackbar>
+
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card class="elevation-12">
+          <v-toolbar color="primary" dark flat>
+            <v-toolbar-title>Login</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-tooltip bottom>
+              <template v-slot:="{ on }">
+                <v-btn :href="source" icon large target="_blank" v-on="on">
+                  <v-icon>mdi-code-tags</v-icon>
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="User Name"
+                name="login"
+                prepend-icon="mdi-account"
+                type="text"
+                v-model="credentials.userId"
+              ></v-text-field>
+
+              <v-text-field
+                id="password"
+                label="Password"
+                name="password"
+                prepend-icon="mdi-lock"
+                type="password"
+                v-model="credentials.password"
+                v-on:keyup.enter="attempt_login"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn v-on:click="attempt_login" color="primary">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Configuration } from "../runtime";
+import { RoomplaApi } from "../apis";
+
+export default Vue.extend({
+  data() {
+    return {
+      credentials: {
+        userId: null,
+        password: null
+      },
+      message: {
+        show: false,
+        text: ""
+      }
+    };
+  },
+  methods: {
+    attempt_login: function(event) {
+      let api = new RoomplaApi();
+      let result = api.loginPost({
+        credentials: this.credentials
+      });
+      result.then(
+        response => {
+          this.message.show = false;
+
+          this.$emit("logged-in", response);
+        },
+        reason => {
+          this.message.text = "Login failed: " + reason.statusText;
+          this.message.show = true;
+          this.credentials.password = null;
+        }
+      );
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
