1274f887a7e31979b4c986d9f8450f534840f320
Thomas Krause <krauseto@hu-berlin.de>
2020-07-12 13:16:14 +0200

    Add test event

--- webapp/src/components/RoomView.vue
+++ webapp/src/components/RoomView.vue
@@ -15,7 +15,13 @@
       </v-toolbar-items>
     </v-toolbar>
 
-    <v-calendar type="week" :first-interval="day_range.start" :interval-count="day_range.count" ></v-calendar>
+    <v-calendar
+      type="week"
+      :events="events"
+      :first-interval="day_range.start"
+      :interval-count="day_range.count"
+      @change="getEvents"
+    ></v-calendar>
   </v-container>
 </template>
 
@@ -26,16 +32,28 @@ export default Vue.extend({
   props: ["room"],
   data() {
     return {
-      day_range:  { start: 7, count: 13 },
+      events: [],
+      day_range: { start: 7, count: 13 },
       day_ranges: [
         { text: "working hours", value: { start: 7, count: 13 } },
         { text: "whole day", value: { start: 0, count: 24 } }
-      ],
-      day_start: 8,
-      day_end: 20
+      ]
     };
   },
-  methods: {}
+  methods: {
+    getEvents({ start, end }) {
+      const events = [];
+
+      events.push({
+        name: "Test event",
+        start: `${start.date}T09:00:00`,
+        end: `${start.date}T14:00:00`,
+        timed: true
+      });
+
+      this.events = events;
+    }
+  }
 });
 </script>
 
