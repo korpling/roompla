d775950d745359bb0a92a9a66c68601ba0e5880c
Thomas Krause <krauseto@hu-berlin.de>
2020-07-16 15:42:01 +0200

    Only build web application if npm feature is active

--- build.rs
+++ build.rs
@@ -1,11 +1,18 @@
-use actix_web_static_files::NpmBuild;
-
+#[cfg(npm)]
 fn build_npm() -> anyhow::Result<()> {
+    use actix_web_static_files::NpmBuild;
     NpmBuild::new("./webapp")
         .install()?
         .run("build")?
         .to_resource_dir()
         .build()?;
+    Ok(())
+}
+
+#[cfg(not(npm))]
+fn build_npm() -> anyhow::Result<()> {
+    use actix_web_static_files::resource_dir;
+    resource_dir("./webapp/dist/").build()?;
     Ok(())
 }
 
