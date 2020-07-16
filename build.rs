94a3ef275a032f7ef62603d5160fec84682ac9b7
Thomas Krause <krauseto@hu-berlin.de>
2020-07-16 15:01:30 +0200

    Build webapp in Rust build script

--- build.rs
+++ build.rs
@@ -1,5 +1,14 @@
-use actix_web_static_files::resource_dir;
+use actix_web_static_files::NpmBuild;
+
+fn build_npm() -> anyhow::Result<()> {
+    NpmBuild::new("./webapp")
+        .install()?
+        .run("build")?
+        .to_resource_dir()
+        .build()?;
+    Ok(())
+}
 
 fn main() {
-    resource_dir("./webapp/dist/").build().unwrap();
+    build_npm().unwrap();
 }
