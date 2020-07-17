0b3881c91da589e2dfaf4abd043e801dfcd16595
Thomas Krause <krauseto@hu-berlin.de>
2020-07-17 16:43:38 +0200

    Align rooms in a grid

--- webapp/src/components/RoomList.vue
+++ webapp/src/components/RoomList.vue
@@ -1,14 +1,16 @@
 <template>
   <v-container fluid>
-    <div v-for="r in store.state.rooms" :key="r.id">
-      <v-card>
-        <v-card-title>{{r.id}}</v-card-title>
-        <v-card-text>{{$tc("people-allowed", r.maxOccupancy, {count: r.maxOccupancy})}}</v-card-text>
-        <v-card-actions>
-           <router-link :to="'/room/' + r.id">{{$t("book-time-slot")}}</router-link>
-        </v-card-actions>
-      </v-card>
-    </div>
+    <v-layout row wrap>
+      <div v-for="r in store.state.rooms" :key="r.id" style="padding:5px;">
+        <v-card min-width="250pt" >
+          <v-card-title>{{r.id}}</v-card-title>
+          <v-card-text>{{$tc("people-allowed", r.maxOccupancy, {count: r.maxOccupancy})}}</v-card-text>
+          <v-card-actions>
+            <router-link :to="'/room/' + r.id">{{$t("book-time-slot")}}</router-link>
+          </v-card-actions>
+        </v-card>
+      </div>
+    </v-layout>
   </v-container>
 </template>
 
@@ -16,11 +18,11 @@
 import Vue from "vue";
 import { Configuration } from "../runtime";
 import { RoomplaApi } from "../apis/RoomplaApi";
-import {store} from "../store";
+import { store } from "../store";
 
 export default Vue.extend({
   data() {
-    return {store: store};
+    return { store: store };
   }
 });
 </script>
